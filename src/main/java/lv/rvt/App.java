6f8108b2f18017f226c3da04c3d8988c397098ae
Sanija Kraukle <166509320+23DP4SKrau@users.noreply.github.com>
2024-10-31 11:51:46 +0000

    save changes

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,25 +4,18 @@ import java.util.*;
 
 public class App 
 {
-    public static void print (String s, int count) {
-        for (int i = 0; i < count; i++) System.out.print(s);
+    public static void printStars(int number) {
+        for(int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 
-    public static void christmasTree(int height) {
-        for (int i = 0; i < height; i++) {
-            print(" ", height - i - 1);
-            print("*", 2 * i + 1);
-            System.out.println();
-        }
-
-        for (int i = 0; i < 2; i++) {
-            print(" ", height - 2);
-            print("*", 3);
-            System.out.println();
-        }
-    }
     public static void main(String[] args) {
-        christmasTree(4);
+        printStars(4);
+        printStars(4);
+        printStars(4);
+        printStars(4);
     }
 }
     
